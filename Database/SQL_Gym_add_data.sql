abb817e06ce3ceb864e1b0a2435227e0af9de4da
CaramelHeaven <srg.fominov@gmail.com>
2018-03-06 10:12:01 +0400

    Updated

---
+++ Database/SQL_Gym_add_data.sql
@@ -0,0 +1,45 @@
+--(id_sale, procent_of_sale)
+insert into Sale values (1, 1);
+insert into Sale values (2, 4);
+insert into Sale values (3, 9);
+insert into Sale values (4, 16);
+insert into Sale values (5, 0);
+
+
+--(id_gym_membership, day_of_start, day_of_end, price_of_membership, sale, type_of_time)
+insert into [Gym Membership] values (1, '2014-4-27', '2014-6-25', 4023, 1, '2 month');
+insert into [Gym Membership] values (2, '2010-1-11', '2018-1-11', 80043, 4, '8 years');
+insert into [Gym Membership] values (3, '2012-8-25', '2013-1-24', 5040, 5, '5 month')
+insert into [Gym Membership] values (4, '2004-1-6', '2007-6-3', 4330, 2, '3 years');
+insert into [Gym Membership] values (5, '2009-3-2', '2013-5-23', 20432, 2, '4 years');
+insert into [Gym Membership] values (6, '2018-3-5', '2020-12-2', 16023, 4, '2 years');
+insert into [Gym Membership] values (7, '2015-4-27', '2016-6-25', 12034, 3, '1 year');
+insert into [Gym Membership] values (8, '2017-11-11', '2018-11-11', 16888, 4, '1 years');
+insert into [Gym Membership] values (9, '2010-8-5', '2013-11-24', 14141, 5, '5 month')
+insert into [Gym Membership] values (10, '2014-1-6', '2016-6-3', 14040, 2, '2 years');
+insert into [Gym Membership] values (11, '2007-3-2', '2013-5-23', 20340, 2, '6 years');
+insert into [Gym Membership] values (12, '2016-3-5', '2022-12-2', 46520, 5, '6 years');
+
+
+
+--id_client, first_name, last_name, gym_membership_id, birthday, bank_account
+insert into [Client Directory] values (1, 'Peter', 'Black', 2, '1985-1-23', '1521-2434-3233-4524');
+insert into [Client Directory] values (2, 'Jack', 'Warthon', 3, '1985-11-6', '1331-6432-3983-4444');
+insert into [Client Directory] values (3, 'Nil', 'White', 1, '1967-12-12', '1421-2222-3643-4854');
+insert into [Client Directory] values (4, 'Semuel', 'Folker', 5, '2000-12-3', '1641-2222-6343-4954');
+insert into [Client Directory] values (5, 'Willy', 'Silk', 4, '1976-5-2', '1261-2222-7443-4444');
+insert into [Client Directory] values (6, 'Dave', 'Morrison', 6, '1984-3-28', '3641-3252-3329-4552');
+insert into [Client Directory] values (7, 'Clara', 'Voer', 8, '2003-1-3', '6383-5234-3533-4524');
+insert into [Client Directory] values (8, 'Rose', 'Jelly', 7, '1975-1-16', '1331-3255-5747-5482');
+insert into [Client Directory] values (9, 'Jilly', 'W', 9, '1969-2-2', '8432-2523-3553-4324');
+insert into [Client Directory] values (10, 'Molly', 'Fier', 10, '2002-2-13', '5474-2222-6343-3254');
+insert into [Client Directory] values (11, 'Rose', 'Deksin', 11, '1988-7-2', '1261-5292-9993-4334');
+insert into [Client Directory] values (12, 'Suzan', 'Xenryd', 12, '1999-3-8', '6371-9632-3759-5232');
+
+
+--id_trainer first_name, last_name, birthday, salary
+insert into [Trainer Directory] values (1, 'Jack', 'Fofeok', 2, '1966-11-23', '2534-1521-4554-4253');
+insert into [Trainer Directory] values (2, 'Genry', 'Felsher', 3, '1955-1-6', '6432-3325-1531-4434');
+insert into [Trainer Directory] values (3, 'Philip', 'Wernel', 1, '1934-2-19', '2532-3633-1561-4325');
+
+
--- Database/SQL_Gym_query_Night.sql
+++ Database/SQL_Gym_query_Night.sql
@@ -1,33 +1,50 @@
-CREATE DATABASE GymDB
+CREATE DATABASE GymDatabase
 COLLATE Cyrillic_General_CI_AS
 GO
 
 --¬се, кто имеют свой id - ставлю id слева, id которые ссылаютс€ на другие id - right
-
+----------------------------------------------------------------------------------------
 CREATE TABLE [Client Directory]
 (
-	id_client int NOT NULL IDENTITY, -- автоинкремитрировано поле. Id јвтоматически генерируютс€
+	id_client int NOT NULL, -- IDENTITY автоинкремитрировано поле. Id јвтоматически генерируютс€ с 1
 	first_name varchar(20) NOT NULL,
 	last_name varchar(20) NOT NULL,
 	gym_membership_id int NOT NULL,
 	birthday date NOT NULL,
-	bank_account int NOT NULL
+	bank_account varchar(16) NOT NULL
 )
 GO
 
+ALTER TABLE [Client Directory]
+ADD CONSTRAINT [PK_Client_Directory] PRIMARY KEY CLUSTERED ([id_client])
+GO
+
+ALTER TABLE [Client Directory]
+WITH CHECK ADD CONSTRAINT [Client_FK] FOREIGN KEY (gym_membership_id)
+REFERENCES [Gym Membership]([id_gym_membership])
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Client Directory]
+CHECK CONSTRAINT [Client_FK]
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE [Trainer Directory]
 (
-	id_trainer int NOT NULL IDENTITY,
+	id_trainer int NOT NULL,
 	first_name varchar(20) NOT NULL,
 	last_name varchar(20) NOT NULL,
 	birthday date NOT NULL,
 	salary money NOT NULL
 )
 GO
-
+ALTER TABLE [Trainer Directory]
+ADD CONSTRAINT [PK_Trainer_Directory] PRIMARY KEY CLUSTERED (id_trainer)
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE [Gym Membership]
 (
-	id_gym_membership int NOT NULL IDENTITY,
+	id_gym_membership int NOT NULL,
 	day_of_start date NOT NULL,
 	day_of_end date NOT NULL,
 	price_of_membership money NOT NULL,
@@ -36,14 +53,31 @@ CREATE TABLE [Gym Membership]
 )
 GO
 
+ALTER TABLE [Gym Membership]
+ADD CONSTRAINT [PK_Gym_Membership] PRIMARY KEY CLUSTERED (id_gym_membership)
+GO
+
+ALTER TABLE [Gym Membership]
+WITH CHECK ADD CONSTRAINT [Gym_FK] FOREIGN KEY (sale)
+REFERENCES Sale(id_sale)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Gym Membership]
+CHECK CONSTRAINT [Gym_FK]
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE Sale
 (
 	id_sale int NOT NULL, 
 	procent_of_sale int NOT NULL,
 )
 GO
-
-CREATE TABLE [Induvidual Schedule]
+ALTER TABLE Sale
+ADD CONSTRAINT [PK_Sale] PRIMARY KEY CLUSTERED (id_sale)
+GO
+----------------------------------------------------------------------------------------
+CREATE TABLE [Individual Schedule]
 (
 	id_individual_work int NOT NULL,
 	time_start time NOT NULL,
@@ -54,9 +88,34 @@ CREATE TABLE [Induvidual Schedule]
 )
 GO
 
+ALTER TABLE [Individual Schedule]
+ADD CONSTRAINT [PK_Individual_Schedule] PRIMARY KEY CLUSTERED (id_individual_work)
+GO
+
+ALTER TABLE [Individual Schedule]
+WITH CHECK ADD CONSTRAINT [IS_fk0_client_id] FOREIGN KEY (client_id)
+REFERENCES [Client Directory](id_client)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Individual Schedule]
+WITH CHECK ADD CONSTRAINT [IS_fk1_trainer_id] FOREIGN KEY (trainer_id)
+REFERENCES [Trainer Directory](id_trainer)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Individual Schedule]
+CHECK CONSTRAINT [IS_fk0_client_id]
+GO
+
+ALTER TABLE [Individual Schedule]
+CHECK CONSTRAINT [IS_fk1_trainer_id]
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE [Group Schedule]
 (
-	id_group int NOT NULL,
+	id_group_schedule int NOT NULL,
+	group_id int NOT NULL,
 	place_id int NOT NULL,
 	trainer_id int NOT NULL,
 	time_start time NOT NULL,
@@ -66,6 +125,52 @@ CREATE TABLE [Group Schedule]
 )
 GO
 
+ALTER TABLE [Group Schedule]
+ADD CONSTRAINT [PK_Group_Schedule] PRIMARY KEY CLUSTERED (id_group_schedule)
+GO
+
+ALTER TABLE [Group Schedule]
+WITH CHECK ADD CONSTRAINT [GS_fk0_group_id] FOREIGN KEY (group_id)
+REFERENCES [Group work](id_group_table)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Group Schedule]
+WITH CHECK ADD CONSTRAINT [GS_fk1_trainer_id] FOREIGN KEY (trainer_id)
+REFERENCES [Trainer Directory](id_trainer)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Group Schedule]
+WITH CHECK ADD CONSTRAINT [GS_fk2_place_id] FOREIGN KEY (place_id)
+REFERENCES Place(id_place)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Group Schedule]
+WITH CHECK ADD CONSTRAINT [GS_fk3_type_of_sport_id] FOREIGN KEY (type_of_sport_id)
+REFERENCES [Kind of Sport](id_kind)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Group Schedule]
+CHECK CONSTRAINT [GS_fk0_group_id]
+GO
+
+ALTER TABLE [Group Schedule]
+CHECK CONSTRAINT [GS_fk1_trainer_id]
+GO
+
+
+ALTER TABLE [Group Schedule]
+CHECK CONSTRAINT [GS_fk2_place_id]
+GO
+
+
+ALTER TABLE [Group Schedule]
+CHECK CONSTRAINT [GS_fk3_type_of_sport_id]
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE [Kind of Sport]
 (
 	id_kind int NOT NULL,
@@ -73,13 +178,22 @@ CREATE TABLE [Kind of Sport]
 )
 GO
 
-CREATE TABLE [Place]
+ALTER TABLE [Kind of Sport]
+ADD CONSTRAINT [PK_Kind_of_Sport] PRIMARY KEY CLUSTERED (id_kind)
+GO
+----------------------------------------------------------------------------------------
+CREATE TABLE Place
 (
 	id_place int NOT NULL,
 	name_place varchar(20) NOT NULL,
 	quantity_place int NOT NULL 
 )
 GO
+
+ALTER TABLE Place
+ADD CONSTRAINT [PK_place] PRIMARY KEY CLUSTERED (id_place)
+GO
+----------------------------------------------------------------------------------------
 
 CREATE TABLE [Group work]
 (
@@ -90,6 +204,20 @@ CREATE TABLE [Group work]
 )
 GO
 
+ALTER TABLE [Group work]
+ADD CONSTRAINT [PK_group_work] PRIMARY KEY CLUSTERED (id_group_table)
+GO
+
+ALTER TABLE [Group work]
+WITH CHECK ADD CONSTRAINT [GW_fk_get_people] FOREIGN KEY (get_people)
+REFERENCES [People Quantity in the group](id)
+ON UPDATE CASCADE
+GO
+
+ALTER TABLE [Group work]
+CHECK CONSTRAINT [GW_fk_get_people]
+GO
+----------------------------------------------------------------------------------------
 CREATE TABLE [People Quantity in the group]
 (
 	id int NOT NULL,
@@ -97,104 +225,16 @@ CREATE TABLE [People Quantity in the group]
 )
 GO
 
-ALTER TABLE [Client Directory]
-ADD PRIMARY KEY (id_client)
-GO
-
-ALTER TABLE [Trainer Directory]
-ADD PRIMARY KEY (id_trainer)
-GO
-
-ALTER TABLE [Gym Membership]
-ADD UNIQUE (id_gym_membership) --ќбеспечиваем уникальность пол€, после этого мы можем св€зать таблицы ClientDirectory with Gym Membership
-GO
-
----------REFENCES----------------------REFENCES----------------------REFENCES----------------------REFENCES----------------------REFENCES-------------
-ALTER TABLE [Client Directory]
-ADD
-FOREIGN KEY (gym_membership_id) REFERENCES [Gym Membership] (id_gym_membership)
-GO
-
-ALTER TABLE [Sale]
-ADD UNIQUE (id_sale)
-GO
-
-ALTER TABLE [Gym Membership]
-ADD
-FOREIGN KEY (sale) REFERENCES [Sale] (id_sale)
-GO
-------------------------------------------------------------------------
-ALTER TABLE [Induvidual Schedule]
-ADD PRIMARY KEY (id_individual_work)
-GO
-
-ALTER TABLE [Induvidual Schedule]
-ADD
-FOREIGN KEY (trainer_id) REFERENCES [Trainer Directory] (id_trainer)
-GO
-
-
-
---ќбеспечение уникальности,
---ALTER TABLE [Client Directory]
---ADD
---UNIQUE(id_client)
---GO
-
--- We set some primaryKeys:
-
---ќ“ЌќЎ≈Ќ»я----ќ“ЌќЎ≈Ќ»я----ќ“ЌќЎ≈Ќ»я----ќ“ЌќЎ≈Ќ»я----ќ“ЌќЎ≈Ќ»я----ќ“ЌќЎ≈Ќ»я--
-
-
-
-ALTER TABLE [Gym Membership]
-ADD
-FOREIGN KEY (sale) REFERENCES [Sale] (id_sale)
-GO
-
-ALTER TABLE [Induvidual Schedule]
-ADD 
-FOREIGN KEY (client_id) REFERENCES [Client Directory] (id_client)
-GO
-
-ALTER TABLE [Induvidual Schedule]
-ADD
-FOREIGN KEY (trainer_id) REFERENCES [Trainer Directory] (id_trainer)
-GO
------------------------------------------------------------
-ALTER TABLE [Group Schedule]
-ADD
-FOREIGN KEY (id_group) REFERENCES [Group work] (id_group_table)
-GO
-
-ALTER TABLE [Group Schedule]
-ADD
-FOREIGN KEY (place_id) REFERENCES [Place] (id_place)
-GO
-
-ALTER TABLE [Group Schedule]
-ADD
-FOREIGN KEY (type_of_sport_id) REFERENCES [Kind Of Sport] (id_kind)
-GO
-
-ALTER TABLE [Group Schedule]
-ADD
-FOREIGN KEY (trainer_id) REFERENCES [Trainer Directory] (id_trainer)
-GO
------------------------------------------------------------
-ALTER TABLE [Group work]
-ADD
-FOREIGN KEY (get_people) REFERENCES [People Quantity in the group] (id)
+ALTER TABLE [People Quantity in the group]
+ADD CONSTRAINT [PK_id_PQIG] PRIMARY KEY CLUSTERED (id)
 GO
 
 ALTER TABLE [People Quantity in the group]
-ADD
-FOREIGN KEY (client) REFERENCES [Client Directory] (id_client)
+WITH CHECK ADD CONSTRAINT [PQIG_fk_client] FOREIGN KEY (client)
+REFERENCES [Client Directory](id_client)
+ON UPDATE CASCADE
 GO
 
---Simple example - ѕри удалении записи Products - у нас так же удалитьс€ запить ProductDetails, т.е. его описание
---ALTER TABLE ProductDetails
---ADD
---FOREIGN KEY(ID) REFERENCES Products(ID)
---ON DELETE CASCADE
---GO
+ALTER TABLE [People Quantity in the group]
+CHECK CONSTRAINT [PQIG_fk_client]
+GO
