9f365f5b6394c557bccfe5ccfdfd96f373e7f0e0
Cat <srg.fominov@gmail.com>
2018-09-15 21:55:44 +0400

    Update and rename RemovalOfChainRules.swift to RemovalOfChainRules.java

---
+++ TranslationMethods/RemovalOfChainRules.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class RemovalOfChainRules {
+
+    private static Map<String, List<String>> map;
+    private static List<String> helper;
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        map = new LinkedHashMap<>();
+        helper = new ArrayList<>();
+
+        System.out.println("Enter grammar, please: ");
+        boolean checking = true;
+        while (checking) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (input.equals("end")) {
+                checking = false;
+            } else {
+                pullingMap(input);
+            }
+        }
+        System.out.flush();
+
+        System.out.println("Entered grammar");
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        provideChainRules(map);
+
+        System.out.println("Deleted chain rules");
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+
+    private static void provideChainRules(Map<String, List<String>> grammar) {
+        for (Map.Entry<String, List<String>> entry : grammar.entrySet()) {
+            helper.clear();
+            for (String rule : entry.getValue()) {
+                if ((rule.length() == 1) && rule.equals(rule.toUpperCase())) {
+                    chainRules(rule, entry.getKey());
+                }
+            }
+            List<String> temp = clearningList(entry.getValue());
+            map.put(entry.getKey(), temp);
+        }
+    }
+
+    private static void chainRules(String rule, String rootKey) {
+        List<String> rules = map.get(rule);
+        for (String temp : rules) {
+            if ((temp.length() == 1) && temp.equals(temp.toUpperCase())) {
+                chainRules(temp, rule);
+            } else {
+                helper.add(temp);
+            }
+        }
+    }
+
+    private static List<String> clearningList(List<String> oldEntry) {
+        List<String> array = new ArrayList<>(oldEntry);
+        List<String> oneLetters = new ArrayList<>();
+        for (String anArray : array) {
+            if ((anArray.length() == 1) && anArray.equals(anArray.toUpperCase())) {
+                oneLetters.add(anArray);
+            }
+        }
+        array.removeAll(oneLetters);
+        array.addAll(helper);
+        Set<String> unique = new LinkedHashSet<>(array);
+        array.clear();
+        array.addAll(unique);
+        return array;
+    }
+
+    private static void pullingMap(String userInput) {
+        userInput = userInput.replaceAll("\\s+", "");
+        String[] parts = userInput.split("->");
+        String key = String.valueOf(parts[0].charAt(0));
+
+        String[] values = parts[1].split("\\|");
+        map.put(key, new ArrayList<>(Arrays.asList(values)));
+    }
+}
