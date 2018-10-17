484962d9710663d24735596ef7d93077f21fc529
Cat <srg.fominov@gmail.com>
2018-10-17 19:10:40 +0400

    fixed some lines

--- InformationSecurity/CRC.swift
+++ InformationSecurity/CRC.swift
@@ -4,12 +4,11 @@ import Foundation
 
 let CRC32: UInt32 = 0x1EDC6F41
 let plusOne: UInt32 = 0x00000001
-let plusZero: UInt32 = 0x00000000
-
 var container: UInt32 = 0xFFFFFFFF
 
 var bytes = [UInt32]()
 
+let mainFile = "/Users/sergeyfominov/Documents/TestDirectory/test2.txt"
 if let data = NSData(contentsOfFile: mainFile) {
 
     var buf = [UInt32](repeating: 0, count: 16)
@@ -17,45 +16,42 @@ if let data = NSData(contentsOfFile: mainFile) {
     bytes = buf
 }
 
-print("buf: \(String(bytes[4], radix: 2))")
 var strContainer = String(container, radix: 2)
 
 var containerOfBites: String = ""
 
 for byte in bytes {
     if byte.leadingZeroBitCount > 0 && byte.leadingZeroBitCount != 32 {
-        print("chek: \(byte.leadingZeroBitCount)")
         let missingZeros = [String](repeating: "0", count: byte.leadingZeroBitCount)
         let bites = missingZeros.joined() + String(byte, radix: 2)
         containerOfBites = containerOfBites + bites
     }
 }
-print(containerOfBites)
+//for _ in 0...32 {
+//    containerOfBites += "0"
+//}
 
 while strContainer.count > 0 {
-    let firstChar = strContainer[..<strContainer.index(strContainer.startIndex, offsetBy: 1)]
-    let overflowChar = String(firstChar)
-
+    let firstChar = strContainer.first
+    let overflowChar = String(firstChar!)
     strContainer.remove(at: strContainer.startIndex)
 
     var addedSymbol = ""
-    if containerOfBites.count > 0 {
+    if containerOfBites.count != 0 {
         let charFromContainerOfBytes = containerOfBites[..<containerOfBites.index(containerOfBites.startIndex, offsetBy: 1)]
         addedSymbol = String(charFromContainerOfBytes)
         strContainer += addedSymbol
         containerOfBites.remove(at: containerOfBites.startIndex)
     }
 
-    if overflowChar == "0" {
-        container = container << 1
-    } else {
-        container = container << 1
-        if addedSymbol == "0" {
-            container = container ^ plusZero
-        } else {
-            container = container ^ plusOne
-            container = container ^ CRC32
-        }
+    container = container << 1
+    
+    if addedSymbol != "" && addedSymbol != "0" {
+        container = container ^ plusOne
+    }
+
+    if overflowChar != "0" {
+        container = container ^ CRC32
     }
 }
 
