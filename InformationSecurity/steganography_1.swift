5690da2e1968888d0006c57ae1546fe5cb96b44c
Cat <srg.fominov@gmail.com>
2018-10-28 13:21:58 +0400

    updated

--- InformationSecurity/steganography_1.swift
+++ InformationSecurity/steganography_1.swift
@@ -1,80 +1,44 @@
 import Foundation
 
 extension String {
-    func between(_ left: String, _ right: String) -> String? {
-        guard let leftRange = range(of: left), let rightRange = range(of: right, options: .backwards)
-            , leftRange.upperBound <= rightRange.lowerBound
-            else { return nil }
-
-        let sub = self[leftRange.upperBound...]
-        let closestToLeftRange = sub.range(of: right)!
-        return String(sub[..<closestToLeftRange.lowerBound])
-    }
-}
-
-func insertHidenText(stroke: String, hidenWord: String, cache: String) -> String {
-    var completedStroke = ""
-    var indicesWord = 1
-    var counter = 0
-    for index in stroke.indices {
-        if stroke[index] != cache[index] && indicesWord == 1 {
-            completedStroke += stroke[..<stroke.index(stroke.startIndex, offsetBy: counter)]
-            completedStroke += hidenWord
-            indicesWord += 1
-            completedStroke += " " + String(stroke.suffix(stroke.count - counter))
-            break
+    typealias Byte = UInt8
+    var hexToBytes: [Byte] {
+        var start = startIndex
+        return stride(from: 0, to: count, by: 2).compactMap { _ in
+            let end = index(after: start)
+            defer { start = index(after: end) }
+            return Byte(self[start...end], radix: 16)
         }
-        counter += 1
     }
-    if completedStroke.last! == " " {
-        completedStroke = String(completedStroke.dropLast())
+    var hexToBinary: String {
+        return hexToBytes.map {
+            let binary = String($0, radix: 2)
+            return repeatElement("0", count: 8 - binary.count) + binary
+        }.joined()
     }
-    return completedStroke
 }
+print("Enter the test, sir")
+var enteredText = readLine()!
+let file = "first.txt"
+var binaryStream = String()
+var lineArray = [String]()
 
-// MAIN
-let file = "test.txt"
-var simpleCache = ""
-var helping = ""
-var hidenText = ""
-var sizeHidenText = 0
-var allText = ""
-print("Enter the instruction, sir ")
-
-let instruction = readLine()
+//ENCODED
+if let data = enteredText.data(using: .windowsCP1251) {
+    let encoded = data.map { String(format: "%02hhX", $0) + "." }.joined()
+    var bytes = encoded.components(separatedBy: ".")
+    bytes.removeLast()
+    for byte in bytes {
+        binaryStream += byte.hexToBinary
+        print("binary: \(byte.hexToBinary)")
+    }
+}
 
 if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-
-    let fileURL = dir.appendingPathComponent(file)
-    hidenText = try String(contentsOf: fileURL, encoding: .utf8)
-    allText = hidenText
-    hidenText = hidenText.between("|", "|")!
+    let allText = try String(contentsOf: dir.appendingPathComponent(file), encoding: .windowsCP1251)
     allText.enumerateLines { (line, _) in
-        if line.contains(hidenText) {
-            simpleCache += line
-            helping = simpleCache.replacingOccurrences(of: " |" + hidenText + "|", with: "")
-            return
-        }
+        lineArray.append(line)
     }
-    allText = allText.replacingOccurrences(of: " |" + hidenText + "|", with: "")
-}
-
-let buf: [UInt8] = Array(hidenText.utf8)
-
-var binaryStream = ""
-
-for kek in buf {
-    if kek.leadingZeroBitCount > 0 {
-        let missingZeros = [String](repeating: "0", count: kek.leadingZeroBitCount)
-        binaryStream += missingZeros.joined() + String(kek, radix: 2)
-    }
-}
-
-sizeHidenText = binaryStream.count
-
-var lineArray = [String]()
-allText.enumerateLines { (line, _) in
-    lineArray.append(line)
 }
 
 for line in lineArray.indices {
@@ -92,78 +56,70 @@ for line in lineArray.indices {
         break
     }
 }
-var myOwnText = ""
-for line in lineArray {
-    myOwnText = myOwnText + line + "\n"
+
+var recordText = ""
+
+for changedLine in lineArray {
+    recordText += changedLine + "\n"
 }
 
 if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-    let fileURL = dir.appendingPathComponent(file)
-
     do {
-        try myOwnText.write(to: fileURL, atomically: false, encoding: .utf8)
+        try recordText.write(to: dir.appendingPathComponent(file), atomically: false, encoding: .windowsCP1251)
     }
-    catch { /* error handling here */ }
+    catch { }
 }
 print("encoded file")
-let ke = readLine()
+
+//DECODED
+print("Press any key for decoded this file")
+let key = readLine()
+lineArray.removeAll()
+var revivalBytes = ""
+var decodedArray = [UInt8]()
+
 if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-
-    var lineArray = [String]()
-    var hidenBytes = ""
-
-    let fileURL = dir.appendingPathComponent(file)
-    hidenText = try String(contentsOf: fileURL, encoding: .utf8)
-    allText = hidenText
-
+    let allText = try String(contentsOf: dir.appendingPathComponent(file), encoding: .windowsCP1251)
     allText.enumerateLines { (line, _) in
         lineArray.append(line)
     }
+}
 
-    var counter = 0
-    for line in lineArray {
-        if counter == 8 {
-            hidenBytes += " "
-            counter = 0
-        }
-        if sizeHidenText > 0 {
-            if String(line.last!) == " " {
-                hidenBytes += "1"
-            } else {
-                hidenBytes += "0"
-            }
-            sizeHidenText -= 1
-            counter += 1
-        }
+var counterByte = 0
+
+for decodedLine in lineArray {
+    if counterByte == 8 {
+        revivalBytes += " "
+        counterByte = 0
     }
-
-    let hidenWord = hidenBytes.split(separator: " ").compactMap {
-        String(Unicode.Scalar(Int($0, radix: 2)!)!)
-    }.joined(separator: "")
-
-    var tem = 0
-    var completed = ""
-    allText.enumerateLines { (line, _) in
-        var myLine = ""
-        if line.contains(helping) && tem == 0 {
-            completed.append(insertHidenText(stroke: line, hidenWord: hidenWord, cache: simpleCache))
-            completed += "\n"
-            tem += 1
-        } else {
-            if String(line.last!) == " " {
-                myLine = line
-                completed.append(String(myLine.dropLast()) + "\n")
-            } else {
-                completed.append(line + "\n")
-            }
-
-        }
+    if String(decodedLine.last!) == " " {
+        revivalBytes += "1"
+    } else {
+        revivalBytes += "0"
     }
-    let url = dir.appendingPathComponent(file)
+    counterByte += 1
+}
 
+revivalBytes = revivalBytes
+    .split(separator: " ")
+    .compactMap {
+        decodedArray.append(UInt8(Int($0, radix: 2)!))
+        return " "
+    }
+    .joined(separator: " ")
+
+var decodedText = String(bytes: decodedArray, encoding: .windowsCP1251)
+print("decoded text: \(decodedText!)")
+
+recordText = ""
+
+for changedLine in lineArray {
+    recordText += changedLine.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) + "\n"
+}
+
+if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
     do {
-        try completed.write(to: url, atomically: false, encoding: .utf8)
+        try recordText.write(to: dir.appendingPathComponent(file), atomically: false, encoding: .windowsCP1251)
     }
-    catch { /* error handling here */ }
-    print("decoded file")
+    catch { }
 }
