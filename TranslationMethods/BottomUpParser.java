9c247aea05e6c3d8bbbf8114a3d737ffc7607a32
Cat <srg.fominov@gmail.com>
2018-11-17 20:48:02 +0400

    Update BottomUpParser.java

--- TranslationMethods/BottomUpParser.java
+++ TranslationMethods/BottomUpParser.java
@@ -4,16 +4,21 @@ public class BottomUpParser {
 
     private static Map<String, List<String>> map;
     private static StringBuilder chainlet;
+    private static StringBuilder cacheChainlet;
     private static List<String> container; // our container -> (q, 1, $, s)
     private static List<String> rulesContainer;
 
     private static String DLR = "$";
-    private static int C = 0;
+    private static int C = 1;
     private static String Q = "q";
     private static String B = "b";
-    private static String chain = "s";
+    private static String chain = "e";
+
+    private static boolean DIRECTION = true; // true = q, false - back
     private static boolean okNext = true;
 
+    private static final int GET_STATUS = 0;
+    private static final int GET_COUNTER = 1;
     private static final int GET_CHAIN = 2;
     private static final int GET_HISTORY = 3;
 
@@ -30,7 +35,7 @@ public class BottomUpParser {
 
         System.out.println(container);
 
-        map.put("S", new ArrayList<>(Arrays.asList("T", "S+T")));
+        map.put("S", new ArrayList<>(Arrays.asList("S+T", "T")));
         map.put("T", new ArrayList<>(Arrays.asList("T*F", "F")));
         map.put("F", new ArrayList<>(Arrays.asList("a")));
 
@@ -38,29 +43,50 @@ public class BottomUpParser {
             rulesContainer.addAll(entry.getValue());
         }
 
-        provideBottomUpParser(map);
+        cacheChainlet = new StringBuilder(chainlet.toString());
+        provideBottomUpParser(map, okNext);
     }
 
-    private static void provideBottomUpParser(Map<String, List<String>> map) {
-        if (okNext) {
+
+    private static void provideBottomUpParser(Map<String, List<String>> map, boolean okNextLetter) {
+        boolean successfulSymbolUp = false;
+        String loadNextChar = "";
+
+        if (okNextLetter) {
             okNext = false;
-            String loadNextChar = String.valueOf(chainlet.charAt(0));
+            loadNextChar = String.valueOf(cacheChainlet.charAt(0));
+            String complexChars = "";
+
+            //add history
+            addHistoryAndCharacter(loadNextChar);
+
+            cacheChainlet.deleteCharAt(0);
+
+            //govnocode
+            String checkLine = container.get(GET_CHAIN).substring(1, container.get(GET_CHAIN).length());
+            if (!checkLine.isEmpty()) {
+                complexChars = checkLine;
+            }
 
             for (Map.Entry<String, List<String>> entry : map.entrySet()) {
                 for (String rule : entry.getValue()) {
-                    if (rule.equals(String.valueOf(loadNextChar))) {
+                    if (rule.equals(String.valueOf(loadNextChar)) || rule.equals(complexChars)) {
                         String key = entry.getKey();
 
-                        String newChain = container.get(GET_CHAIN).replace(container.get(GET_CHAIN), container.get(GET_CHAIN) + key);
+                        //bug below, if u put aa - u will get FF for example
+                        String newChain = container.get(GET_CHAIN).replace(rule, key);
                         container.set(GET_CHAIN, newChain);
 
                         int position = 1 + rulesContainer.indexOf(rule);    // count by key + pos in the list
                         String newHistory = position + container.get(GET_HISTORY); // 5s
                         container.set(GET_HISTORY, newHistory);
 
+                        //show result our steps
+                        System.out.println(container);
 
-                        chainlet.deleteCharAt(0);
-                        provideBottomUpParser(map);
+                        okNext = false;
+                        successfulSymbolUp = true;
+                        provideBottomUpParser(map, okNext);
 
                         System.out.println("equals");
                     } else {
@@ -68,20 +94,82 @@ public class BottomUpParser {
                     }
                 }
             }
+        } else {
+            boolean diveMore = false;
 
+            //Свертка, ласт элемент свернулся и мы смотрим, может ли новая цепь свернуться во что-нибудь больше
             for (Map.Entry<String, List<String>> entry : map.entrySet()) {
                 for (String rule : entry.getValue()) {
                     String cleanUp = container.get(GET_CHAIN).substring(1, container.get(GET_CHAIN).length());
+                    String lastSymbol = cleanUp.substring(cleanUp.length() - 1);
 
                     if (cleanUp.equals(rule)) {
                         int position = 1 + rulesContainer.indexOf(rule);    // count by key + pos in the list
                         String newHistory = position + container.get(GET_HISTORY); // ...s
                         container.set(GET_HISTORY, newHistory);
 
-                        provideBottomUpParser(map);
+                        String newChain = container.get(GET_CHAIN).replace(
+                                container.get(GET_CHAIN), "$" + entry.getKey()
+                        );
+                        container.set(GET_CHAIN, newChain);
+
+                        //show result our steps
+                        System.out.println(container);
+
+                        diveMore = true;
+                        provideBottomUpParser(map, false);
+                    } else if (rule.equals(lastSymbol)) {
+                        int position = 1 + rulesContainer.indexOf(rule);    // count by key + pos in the list
+                        String newHistory = position + container.get(GET_HISTORY); // ...s
+                        container.set(GET_HISTORY, newHistory);
+
+                        String newChain = container.get(GET_CHAIN).replace(lastSymbol, entry.getKey());
+                        container.set(GET_CHAIN, newChain);
+
+                        //show results
+                        System.out.println(container);
+
+                        diveMore = true;
+                        provideBottomUpParser(map, false);
+                    } else {
+                        System.out.println("i'm here");
                     }
                 }
             }
+
+            //if chains equals nil and we come back
+            if (cacheChainlet.length() == 0) {
+                container.set(GET_STATUS, "b");
+                DIRECTION = false;
+            }
+
+            //if наш дайв во всех правилах не нашел продолжения, мы добавляем next symbol and provide next step
+            if (!diveMore && DIRECTION) {
+                provideBottomUpParser(map, true);
+            }
         }
+
+        //clean code
+        if (!successfulSymbolUp && DIRECTION) {
+            if (!loadNextChar.isEmpty()) {
+                provideBottomUpParser(map, true);
+            }
+        }
+    }
+
+    private static void addHistoryAndCharacter(String loadNextChar) {
+        container.set(GET_CHAIN, container.get(GET_CHAIN) + loadNextChar);
+
+        C += 1;
+        container.set(GET_COUNTER, String.valueOf(C));
+
+        if (container.get(GET_HISTORY).equals("e") && container.get(GET_CHAIN).length() > 1) {
+            container.set(GET_HISTORY, "s");
+        } else {
+            StringBuilder builder = new StringBuilder(container.get(GET_HISTORY)).insert(0, "s");
+            container.set(GET_HISTORY, builder.toString());
+        }
+
+        System.out.println(container);
     }
 }
