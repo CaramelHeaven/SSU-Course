55ac6e0afc0b279e8c593061c0ab9caefb0990fe
Cat <srg.fominov@gmail.com>
2019-02-08 11:22:13 +0400

    Update polybius_square.swift

--- Cryptography/polybius_square.swift
+++ Cryptography/polybius_square.swift
@@ -56,9 +56,6 @@ struct Matrix {
             }
         }
 
-        if row == 0 && column == 0 {
-            return (String(self.rows), String(self.columns))
-        }
         return (String(row), String(column))
     }
 
@@ -132,104 +129,123 @@ func buildMatrix(alphabet: String, keyMain: String) -> Matrix {
     return matrix
 }
 
-func checkingEnterUserText(str: String) -> Bool {
-    return true
-}
-
 //initial data
 var alphabet = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю"
+var enteringText = alphabet
 alphabet = String(alphabet.sorted()) + "0123456789.,:-!? "
 
 //MARK - MAIN
 
-
-
-var isOutFromLoop = false
-
-while(!isOutFromLoop) {
-    print("Choose action")
-    print("1 - encoded data")
-    print("2 - decoded data")
-    let action = readLine()
-
-    if action == "1" {
-        // MARK - ENCODER
-
-        print("Enter you key")
-        var keyMain = readLine()!
-        refactoringString(str: &keyMain)
-
-        print("Enter you text for encode")
-        let textFromUser = readLine()!
-
-        var encodeText = ""
-
-        let matrix = buildMatrix(alphabet: alphabet, keyMain: keyMain)
-        matrix.sout()
-
-        print("suka \(matrix[0, 0])")
-
-        for char in textFromUser {
-            let (row, column) = matrix.findCharacterPosition(single: String(char)) //haha
-            encodeText += (row + column)
-        }
-
-        print("encoded: \(encodeText)")
-
-        //Create file and save data
-        if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
-            do {
-                try encodeText.write(to: dir.appendingPathComponent("output.txt"), atomically: false, encoding: .utf8)
-            }
-            catch {
-                print("something error: \(error)")
-            }
-        }
-
-        print("Do u want to decode file? yes/no")
-        let kek = readLine()!
-        if kek != "yes" {
-            isOutFromLoop = !isOutFromLoop
-        } else {
-            print("continue")
-        }
-    } else {
-        // MARR - ECONDER
-
-        print("Enter a key for decode file")
-        var keyMain = readLine()!
-        refactoringString(str: &keyMain)
-
-        var outputText = ""
-        var decodedText = ""
-
-        if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
-            outputText = try String(contentsOf: dir.appendingPathComponent("output.txt"), encoding: .utf8)
-        }
-
-        let matrix = buildMatrix(alphabet: alphabet, keyMain: keyMain)
-
-        matrix.sout()
-
-        var pairsArray = [String]()
-
-        while outputText.count > 1 {
-            var pair = String(outputText.remove(at: outputText.startIndex))
-            pair += String(outputText.remove(at: outputText.startIndex))
-
-            pairsArray.append(pair)
-        }
-
-        print("pairs: \(pairsArray)")
-
-        for item in pairsArray {
-            let char = matrix.findCharByPositionInMatrix(row: Int(String(item.first!))!, column: Int(String(item.last!))!)
-
-            decodedText += char
-        }
-
-        print("Decoded: \(decodedText)")
-
-        isOutFromLoop = !isOutFromLoop
+func checkingEnterUserKey(str: String) -> Bool {
+    for item in str {
+        if !alphabet.contains(item) {
+            return false
+        }//Алалалалалал
     }
+    return true
+}
+
+func checkingEnterUserText(string: String) -> Bool {
+    print("checkign string: \(string)")
+    for item in string {
+        if !enteringText.contains(item) {
+            return false
+        }
+    }
+    return true
+}
+
+
+print("Choose action")
+print("1 - encoded data")
+print("2 - decoded data")
+let action = readLine()
+
+if action == "1" {
+    // MARK - ENCODER
+
+    print("Enter you key")
+    var keyMain = readLine()!
+
+    while(!checkingEnterUserKey(str: keyMain)) {
+        print("You entered non compatible key, try again: ")
+        keyMain = readLine()!
+    }
+
+    refactoringString(str: &keyMain)
+
+    print("Enter you text for encode")
+    var textFromUser = readLine()!
+
+    while(!checkingEnterUserText(string: textFromUser)) {
+        print("You entered non compatible text, try again: ")
+        textFromUser = readLine()!
+    }
+
+    var encodeText = ""
+
+    let matrix = buildMatrix(alphabet: alphabet, keyMain: keyMain)
+    matrix.sout()
+
+    for char in textFromUser {
+        let (row, column) = matrix.findCharacterPosition(single: String(char)) //haha
+        encodeText += (row + column)
+    }
+
+    print("encoded: \(encodeText)")
+
+    //Create file and save data
+    if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+        do {
+            try encodeText.write(to: dir.appendingPathComponent("output.txt"), atomically: false, encoding: .utf8)
+        }
+        catch {
+            print("something error: \(error)")
+        }
+    }
+
+} else if action == "2" {
+    // MARR - ECONDER
+
+    print("Enter a key for decode file")
+    var keyMain = readLine()!
+
+    while(!checkingEnterUserKey(str: keyMain)) {
+        print("You entered non compatible text, enter again: ")
+        keyMain = readLine()!
+    }
+
+    refactoringString(str: &keyMain)
+
+    var outputText = ""
+    var decodedText = ""
+
+    if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+        outputText = try String(contentsOf: dir.appendingPathComponent("output.txt"), encoding: .utf8)
+    }
+
+    let matrix = buildMatrix(alphabet: alphabet, keyMain: keyMain)
+
+    matrix.sout()
+
+    var pairsArray = [String]()
+
+    while outputText.count > 1 {
+        var pair = String(outputText.remove(at: outputText.startIndex))
+        pair += String(outputText.remove(at: outputText.startIndex))
+
+        pairsArray.append(pair)
+    }
+
+    print("pairs: \(pairsArray)")
+
+    for item in pairsArray {
+        let char = matrix.findCharByPositionInMatrix(row: Int(String(item.first!))!, column: Int(String(item.last!))!)
+
+        decodedText += char
+    }
+
+    print("Decoded: \(decodedText)")
+} else {
+    print("Exit from programm")
 }
