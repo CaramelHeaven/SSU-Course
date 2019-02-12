6633fda8be1d906b4146d9a09bd302564727e5ae
Cat <srg.fominov@gmail.com>
2019-02-12 20:43:47 +0400

    completed

--- Cryptography/cipherDora.swift
+++ Cryptography/cipherDora.swift
@@ -15,6 +15,15 @@ englishAlphabet = String(englishAlphabet.sorted())
 
 var baseTable = [[Int]: String]() // our table encoding
 
+extension String {
+    public func index(of char: Character) -> Int? {
+        if let idx = characters.index(of: char) {
+            return characters.distance(from: startIndex, to: idx)
+        }
+        return nil
+    }
+}
+
 func setRemainLetters(keyLetters: String) {
     for item in keyLetters {
         if alphabetForWork.contains(item) {
@@ -34,7 +43,7 @@ func cleanUpUserText(text: inout String) {
 func removeUselessKeyLetters(key: String) -> String {
     var refactorKey = ""
     var counter = 0 // counter which we need to count first 9 symbols from key
-    var fakeAlphabet = alphabetForWork
+    var fakeAlphabet = englishAlphabet
 
     for char in key {
         if fakeAlphabet.contains(char) {
@@ -56,6 +65,15 @@ func removeUselessKeyLetters(key: String) -> String {
     return refactorKey
 }
 
+//sout baseTable
+func sout() {
+    let kek = baseTable.sorted { $0.key.count > $1.key.count }
+
+    for (item, value) in kek {
+        print("\(item) : \(value)")
+    }
+}
+
 func fillTable(key: String) {
     let firstLine = [4, 5, 6, 7, 8, 9]
     let secondLine = [2, 3]
@@ -71,8 +89,40 @@ func fillTable(key: String) {
     baseTable[thirdLine] = secondHalf
 }
 
+func encodingByTable(char: Character) -> (String?, String?) {
+    for (key, value) in baseTable {
+        if value.contains(char) {
+            let rowKey = key.randomElement()!
+            let columnKey = value.index(of: char)! + 1 //we need to plus 1 for better result
+
+            return (String(rowKey), String(columnKey))
+        }
+    }
+
+    return (nil, nil)
+}
+
+func decodingByTable(pair: String) -> String? {
+    let first = Int(String(pair.first!))!
+    let second = Int(String(pair.last!))!
+
+    for (key, value) in baseTable {
+        if key.contains(first) {
+            let index = value.index(value.startIndex, offsetBy: (second - 1)) // we need to minus 1. @see encodingByTable
+            return String(value[index])
+        }
+    }
+
+    return nil
+}
+
+
 var key = ""
 var text = ""
+var output = ""
+
+var encoding = ""
+var decoding = ""
 
 if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
     key = try String(contentsOf: dir.appendingPathComponent("key 2.txt"), encoding: .utf8)
@@ -82,14 +132,59 @@ if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMas
     text = text.lowercased()
 
     key = removeUselessKeyLetters(key: key) // get refactoring key
-    print(key)
-    print(alphabetForWork)
+    cleanUpUserText(text: &text)
 
     fillTable(key: key)
 
-    for (item, value) in baseTable {
-        print("item \(item) value \(value)")
+    sout()
+
+    for char in text {
+        let (row, column) = encodingByTable(char: char)
+        encoding += row! + column!
+
+        let fantom = arc4random_uniform(2)
+        if fantom == 0 {
+            encoding += String(fantom)
+        }
     }
 
-    
+    if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+        do {
+            try encoding.write(to: dir.appendingPathComponent("output.txt"), atomically: false, encoding: .utf8)
+        }
+        catch {
+            print("something error: \(error)")
+        }
+    }
+}
+
+if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+    key = try String(contentsOf: dir.appendingPathComponent("key 2.txt"), encoding: .utf8)
+    output = try String(contentsOf: dir.appendingPathComponent("output.txt"), encoding: .utf8)
+
+    key = key.lowercased()
+    key = removeUselessKeyLetters(key: key)
+    output = output.replacingOccurrences(of: "0", with: "")
+
+    fillTable(key: key)
+
+    sout()
+
+    var pairsArray = [String]()
+
+    while output.count > 1 {
+        var pair = String(output.remove(at: output.startIndex))
+        pair += String(output.remove(at: output.startIndex))
+
+        pairsArray.append(pair)
+    }
+
+    for pair in pairsArray {
+        let letter = decodingByTable(pair: pair)
+
+        decoding += letter!
+    }
+
+    print("textus: \(text)")
+    print("output: \(decoding)")
 }
